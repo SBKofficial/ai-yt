ba69cf43ee873831369ec84c704b9f8165892b67
SBKofficial <86524089+SBKofficial@users.noreply.github.com>
2025-11-29 01:44:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 # Import your helper scripts
-from free_creator import generate_free_script, get_viral_topic
+from creator import generate_free_script, get_viral_topic
 from free_artist import download_free_images
 from free_audio import generate_voiceover
 from editor import create_video
@@ -118,3 +118,4 @@ def run_once():
 
 if __name__ == "__main__":
     run_once()
+
